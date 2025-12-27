a15841e9c69ad51a336a5dfc775b6d099a25d797
sri-0908 <srilakshmianil2006@gmail.com>
2025-12-27 09:28:32 +0530

    Update main.py

--- FraudEye/app/main.py
+++ FraudEye/app/main.py
@@ -8,3 +8,7 @@ app.include_router(router, prefix="/api")
 @app.get("/")
 def root():
     return {"status": "FraudEye backend running"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
